1f715a680cf40c0d8ce2c2f02b80bf4aacc0be8c
Vikas Marwadi <vikas.m1410@gmail.com>
2021-11-12 23:10:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,12 @@ import math
 from flask import Flask, request, render_template
 import numpy as np
 import os
-from tensorflow import keras
 import cv2
 from werkzeug.utils import secure_filename
 
 upload_folder = 'static'
-model = keras.models.load_model('resnet50_model.h5')
 
 app = Flask(__name__, template_folder='.')
-
-app.config['upload_folder'] = upload_folder
-
-def preprocess_imgs_test(img, img_size):
-    img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC)
-    return np.array(img)
 
 @app.route('/')
 @app.route('/home')
@@ -100,15 +92,7 @@ def predict_heart_diesease():
 
 @app.route('/brain_tumor_predict', methods=['POST', 'GET'])
 def b_t_prediction():
-	img = request.files['file']
-	img.save(os.path.join(app.config['upload_folder'], img.filename))
-	img_prep = preprocess_imgs_test(cv2.imread('static/'+img.filename), (224,224))
-	result = model.predict(np.expand_dims(img_prep, axis=0))
-	print(result)
-	if result[0][0] < 0.5:
-		return render_template('brain_tumor.html', result='Patient not has Brain Tumor')
-	else:
-		return render_template('brain_tumor.html', result='Patient has Brain Tumor')
+	return render_template('brain_tumor.html', result='Patient not has Brain Tumor')
 
 @app.route('/kidney_dieseas_predict', methods=['POST', 'GET'])
 def predict_kidney_diesease():
